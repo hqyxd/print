766338a47cabb67b8bfb49e0dd87312b4771527a
hanqy <1015532632@qq.com>
2022-11-21 13:50:34 +0800

    优化

--- src/main/java/com/config/LogPrintAspect.java
+++ src/main/java/com/config/LogPrintAspect.java
@@ -11,6 +11,8 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -39,9 +41,10 @@ public class LogPrintAspect {
         log.info("请求类名:{},请求方法名:{}",
                 point.getSignature().getDeclaringTypeName(),
                 point.getSignature().getName());
-        log.info("请求URL:{},请求 IP:{},请求参数:{}",
+        log.info("请求URL:{},请求 IP:{},请求头参数:{},请求参数:{}",
                 request.getRequestURL(),
                 request.getRemoteAddr(),
+                JSONObject.toJSONString(getHeaders(request)),
                 JSONObject.toJSONString(parameterMap));
         long beginTime = System.currentTimeMillis();
         //执行时长(毫秒)
@@ -50,5 +53,16 @@ public class LogPrintAspect {
         log.info("执行时长(毫秒):{}", time);
         log.info("接口返回值:{}", JSONObject.toJSONString(result));
         return result;
+    }
+
+    private Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> headerMap = new HashMap<>();
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            headerMap.put(name, value);
+        }
+        return headerMap;
     }
 }
